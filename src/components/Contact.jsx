9549dcc372478a3836b9b1a3f7496f80b37446ad
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-01-06 14:43:57 +0100

    contact heading

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,24 @@
 const Contact = () => {
     return ( <>
 
-<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+<div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6" id="contact">
+
+      
+<h1 class=" text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Contact</h1>
+<p class=" text-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Are you a model who values consistency, and want to work with us?</p>
+
+
 
 <form>
   <div class="mb-6">
-  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name:</label>
     <input type="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Anna Example"/>
     </div>
     <div class="mb-6">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email:</label>
     <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="anna-example@outlook.com" required/>
 </div>
-    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message:</label>
 <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a message..."></textarea>
 
 <div class="mt-3">
