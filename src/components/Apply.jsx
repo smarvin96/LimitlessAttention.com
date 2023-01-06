f050905abf6a3ea5cc29d193e4787abee1cd2aa2
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-01-06 16:35:30 +0100

    form

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -20,22 +20,18 @@ const Apply = () => {
 
 
 
+
     <div class="mb-6">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com"/>
-</div>
-
-
+  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">How can we reach you the best? *</label>
+    <input type="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Instagram/Telegram/WhatsApp/Email"required/>
+    </div>
 
 <div class="mb-6">
   <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Link to social media *</label>
     <input type="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What should we call you?"/>
     </div>
 
-    <div class="mb-6">
-  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">How can we reach you the best? *</label>
-    <input type="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Instagram/Telegram/WhatsApp"required/>
-    </div>
+ 
 
     <div class="mb-6">
   <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Where are you from? *</label>
@@ -98,7 +94,7 @@ const Apply = () => {
 
 
 <div class="mb-6">
-    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Do you have already a verified OnlyFans / Fansly / something else?</label>
+    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Do you have already a verified OnlyFans / Fansly / DarkFans/ something else? *</label>
     <div class="flex items-center mb-4">
     <input id="language" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
     <label for="language" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
@@ -150,8 +146,9 @@ const Apply = () => {
     <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">What else should we know?</label>
 <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>
 
-<div class="mt-3">
-  <button type="submit" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Apply</button>
+<div class="mt-3 text-center">
+  
+  <button type="submit" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Apply</button>
   </div>
 </form>
 </div>
