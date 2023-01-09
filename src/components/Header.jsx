f359b7d42ade0bad245d0ac78054feb1549c3965
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-01-09 13:22:41 +0100

    save2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -75,7 +75,7 @@ const Header = () => {
               <li>
                 <a
                   href="#contact"
-                  className=""
+                  className="text-lg block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
                 </a>
@@ -83,7 +83,7 @@ const Header = () => {
               <li>
                 <a
                   href="#faqs"
-                  className=""
+                  className="text-lg block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   FAQs
                 </a>
@@ -94,10 +94,6 @@ const Header = () => {
 
         </div>
       </nav>
-      <section id="benefits">benefit</section>
-          <section id="testimonials">testimonials</section>
-          <section id="contact">contact</section>
-          <section id="faqs">faqs</section>
 
     </>
   );
