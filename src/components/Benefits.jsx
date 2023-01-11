6fc66926ca75d601794d3de302f482dccfebd758
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-01-11 12:58:19 +0100

    circleBackground

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,60 +1,176 @@
 const Benefits = () => {
-    return ( <>
-    <section className="bg-white dark:bg-gray-900 py-8 " id="benefits">
-    <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 mt-10">
-        <div className="max-w-screen-md mb-8 lg:mb-16">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Struggling to grow your OF?</h2>
-            <p className=" text-lg text-gray-700 sm:text-xl dark:text-gray-400">Take advantage of our expert's years of experience to launch you in to the highest earners on Onlyfans 🚀</p>
-        </div>
-        <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+  return (
+    <>
+      <section className="bg-white dark:bg-gray-900 py-8 " id="benefits">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 mt-10">
+          <div className="max-w-screen-md mb-8 lg:mb-16">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Struggling to grow your OF?
+            </h2>
+            <p className=" text-lg text-gray-700 sm:text-xl dark:text-gray-400">
+              Take advantage of our expert's years of experience to launch you
+              in to the highest earners on Onlyfans 🚀
+            </p>
+          </div>
+          <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
             <div id="benefits:skyrocket">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-300 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="purple" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd"></path></svg>
-                </div>
+              <div className="border flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-purple-300 lg:h-12 lg:w-12">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="purple"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path>
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
 
-                <h3 className="mb-2 text-xl font-bold dark:text-white">Skyrocket earnings</h3>
-                <p className="text-gray-500 dark:text-gray-400">Our dedicated team of experienced professionals,<br/> conduct an indepth analysis of your niche and socials. <br/> We make a plan, then execute to ensure maximum growth.</p>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Skyrocket earnings
+              </h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Our dedicated team of experienced professionals,
+                <br /> conduct an indepth analysis of your niche and socials.{" "}
+                <br /> We make a plan, then execute to ensure maximum growth.
+              </p>
             </div>
             <div id="benefits:focus">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="purple" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd"></path></svg>
-                </div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full  bg-purple-300  lg:h-12 lg:w-12 ">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="purple"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
 
-                <h3 className="mb-2 text-xl font-bold dark:text-white">Focus on creating</h3>
-                <p className="text-gray-500 dark:text-gray-400">You're good at pleasing your fans. <br/>You shouldn't have to worry about all the boring tasks that take up more than 80% of your time. Life becomes a lot less stressful with us.</p>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Focus on creating
+              </h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                You're good at pleasing your fans. <br />
+                You shouldn't have to worry about all the boring tasks that take
+                up more than 80% of your time. Life becomes a lot less stressful
+                with us.
+              </p>
             </div>
             <div id="benefits:no-more-chatting">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="purple" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path><path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path></svg>
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">No more chatting</h3>
-                <p className="text-gray-500 dark:text-gray-400">No more looking at the images you'd rather not see.<br/> You know the one's we're talking about.<br/> We've trained our team to talk like you, but more profitably.</p>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-purple-300  lg:h-12 lg:w-12 ">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="purple"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path>
+                  <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                No more chatting
+              </h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                No more looking at the images you'd rather not see.
+                <br /> You know the one's we're talking about.
+                <br /> We've trained our team to talk like you, but more
+                profitably.
+              </p>
             </div>
             <div id="benefits:privacy">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="purple" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clipRule="evenodd"></path><path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"></path></svg>
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">Privacy</h3>
-                <p className="text-gray-500 dark:text-gray-400">We pride ourselves on having the highest data security standards in the industry. <br/> You also don't worry about your content being leaked.<br/> Our team of IT and legal professionals actively fight plagiarists.</p>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-purple-300  lg:h-12 lg:w-12 ">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="purple"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
+                    clipRule="evenodd"
+                  ></path>
+                  <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Privacy
+              </h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                We pride ourselves on having the highest data security standards
+                in the industry. <br /> You also don't worry about your content
+                being leaked.
+                <br /> Our team of IT and legal professionals actively fight
+                plagiarists.
+              </p>
             </div>
             <div id="benefits:reclaim-time">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="purple" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"></path></svg>
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">Reclaim your time</h3>
-                <p className="text-gray-500 dark:text-gray-400">Most models spend weeks if not months trying to make a living on OF.<br/> It can be worrying not knowing if you'll make it.<br/> Skip all of that and let us take you there in no time at all.</p>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-purple-300  lg:h-12 lg:w-12 ">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="purple"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Reclaim your time
+              </h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Most models spend weeks if not months trying to make a living on
+                OF.
+                <br /> It can be worrying not knowing if you'll make it.
+                <br /> Skip all of that and let us take you there in no time at
+                all.
+              </p>
             </div>
             <div id="benefits:professionals">
-                <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="purple" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd"></path></svg>
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">Professionals on your side</h3>
-                <p className="text-gray-500 dark:text-gray-400">Any problem you have regarding Onlyfans, we'll take care of. <br/>Imagine having someone who can fix anything just a phone call away. That's us. <br/> Our dedicated team is looking out for you.</p>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-purple-300  lg:h-12 lg:w-12 ">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="purple"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Professionals on your side
+              </h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Any problem you have regarding Onlyfans, we'll take care of.{" "}
+                <br />
+                Imagine having someone who can fix anything just a phone call
+                away. That's us. <br /> Our dedicated team is looking out for
+                you.
+              </p>
             </div>
+          </div>
         </div>
-    </div>
-</section>
-    </> );
-}
- 
+      </section>
+    </>
+  );
+};
+
 export default Benefits;
